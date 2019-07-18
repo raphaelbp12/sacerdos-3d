cd80c8064f40e416ccf7dd712815632f57625fd5
Raphael Parreira <raphaelbp12@gmail.com>
2019-07-18 07:17:52 -0300

    Camera Improved

--- Sacerdos/Assets/Scripts/Core/FollowCamera.cs
+++ Sacerdos/Assets/Scripts/Core/FollowCamera.cs
@@ -7,9 +7,19 @@ namespace Scrds.Core
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] Transform target;
+
+        [SerializeField] float heightOffset = 7;
+        [SerializeField] float zOffset = 4;
+        private Camera mainCamera;
+
+        private void Start() {
+            mainCamera = Camera.main;
+            mainCamera.transform.rotation = Quaternion.Euler(60, 0, 0);
+        }
         
         void LateUpdate()
         {
+            mainCamera.transform.position = target.position + new Vector3(0, heightOffset, -zOffset);
             transform.position = target.position;
         }
     }
