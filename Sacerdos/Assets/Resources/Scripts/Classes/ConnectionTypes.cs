7378a529dfd1c7099bd0117985669c26608328f7
Raphael Parreira <rbp@blue-ocean-robotics.com>
2020-04-04 01:16:09 -0300

    tile found

---
+++ Sacerdos/Assets/Resources/Scripts/Classes/ConnectionTypes.cs
@@ -0,0 +1,5 @@
+﻿public enum ConnectionTypes {
+    empty,
+    a,
+    b
+}
---
+++ Sacerdos/Assets/Resources/Scripts/Core/TileGeneratorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGeneratorController : MonoBehaviour
+{
+    public Object[] allTiles;
+    public GameObject tileFound;
+    public int width = 3;
+    public int height = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = Resources.LoadAll("Game/World/Tiles", typeof(GameObject));
+        tileFound = SearchTile(ConnectionTypes.empty, ConnectionTypes.empty);
+    }
+
+    GameObject SearchTile(ConnectionTypes first, ConnectionTypes second)
+    {
+        foreach (Object tile in allTiles)
+        {
+            bool hasFirst = false;
+            TileController tileController = ((GameObject)tile).GetComponent<TileController>();
+            foreach (ConnectionTypes connection in tileController.connections)
+            {
+                if (!hasFirst && connection == first) {
+                    hasFirst = true;
+                    continue;
+                }
+                
+                if (hasFirst && connection == second) {
+                    return ((GameObject)tile);
+                }
+            }
+        }
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
