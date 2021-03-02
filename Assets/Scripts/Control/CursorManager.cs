3f429d7ffadeeb2521513d42c355a39439b9d182
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2021-03-02 11:53:12 -0300

    Refactor: Extract CursorManager class from PlayerController

---
+++ Assets/Scripts/Control/CursorManager.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scrds.Control
+{
+    public enum CursorType
+    {
+        None,
+        Movement,
+        Combat,
+        GUI
+    }
+
+    [System.Serializable]
+    public struct CursorMapping
+    {
+        public CursorType type;
+        public Texture2D texture;
+        public Vector2 hotspot;
+    }
+
+    public class CursorManager
+    {
+        IDictionary<CursorType, CursorMapping> _cursors = new Dictionary<CursorType, CursorMapping>();
+        CursorMapping _default;
+        public CursorManager(IList<CursorMapping> cursors)
+        {
+            foreach (CursorMapping cursor in cursors) {
+                _cursors.Add(cursor.type, cursor);
+            }
+
+            if (_cursors.TryGetValue(CursorType.None, out CursorMapping noneCursor)) {
+                _default = noneCursor;
+            } else {
+                _default = new CursorMapping();
+            }
+        }
+
+        public void SetCursor(CursorType type)
+        {
+            if (_cursors.TryGetValue(type, out CursorMapping cursor)) {
+                Cursor.SetCursor(cursor.texture, cursor.hotspot, CursorMode.Auto);
+            } else {
+                Cursor.SetCursor(_default.texture, _default.hotspot, CursorMode.Auto);
+            }
+        }
+    }
+}
